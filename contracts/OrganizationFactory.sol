b3576a3f896f8495dc19807fbb65914bdcc5de1e
thuanndqsoft <thaophuong.trinh.9@gmail.com>
2022-05-24 00:09:15 +0700

    feat: init truffle

---
+++ contracts/OrganizationFactory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.4.23 <0.9.0;
+
+import './Organization.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/proxy/Clones.sol";
+
+contract OrganizationFactory is Ownable{
+
+  Organization[] public _organizations;
+
+  event OrganizationCreated(Organization newOrganization);
+
+
+  function createOrganization (string memory _name) public {
+    Organization newOrganization = new Organization(_name, msg.sender);
+    _organizations.push(newOrganization);
+    emit OrganizationCreated(newOrganization);
+  }
+
+  function allOrganizations () public view returns (Organization[] memory ){
+    return _organizations;
+  }
+}
