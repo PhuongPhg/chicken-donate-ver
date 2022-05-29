2c72c6dc4fb446d40c77cf6b78f75e04becc720e
theanh098 <mtheanh2910@gmail.com>
2022-05-29 12:04:39 +0700

    update recent donor

--- src/components/ProfileDetail/RecentHistory.tsx
+++ src/components/ProfileDetail/RecentHistory.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './style.module.scss';
+import { getDonor } from 'service';
+import { IDonor } from 'types/donor';
+import moment from 'moment';
 
-interface IRecentHistory {
-  name: string;
-  text: string;
+export interface IRecentHistory {
+  donorId: string;
   amount: string;
-  time: string;
+  time: number;
 }
 
-function RecentHistory() {
+function RecentHistory(props: IRecentHistory) {
+  const { amount, donorId, time } = props;
+  const [donor, setDonor] = useState<IDonor>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getDonor(donorId);
+      setDonor(res);
+    };
+    fetchData();
+  }, [donorId]);
   return (
     <div className={classes.wrapper}>
       <div className={classes.dot}></div>
       <div>
-        <span>ahuhu</span>
-        <span> donated 3eggs</span>
-        <div>2020-6-7 20:00</div>
+        <span style={{ fontWeight: 700 }}>{donor?.name}</span>
+        <span>
+          {' '}
+          donated <span style={{ fontWeight: 700 }}>{amount}</span> ETH
+        </span>
+        <div>{moment.unix(time).toString()}</div>
       </div>
     </div>
   );
