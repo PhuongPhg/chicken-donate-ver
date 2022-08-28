147c070294c6ffa31e21ea079ac8c6c56ec4210a
PhuongPhg <thaophuong.trinh.9@gmail.com>
2022-08-28 23:42:26 +0700

    fix: add routes

--- src/routes/AppRoutes/index.tsx
+++ src/routes/AppRoutes/index.tsx
@@ -5,14 +5,19 @@ import Layout from 'layout/DefaultLayout';
 import Home from 'pages/Home';
 import Profile from 'pages/Profile';
 import Creation from 'pages/Creation';
+import About from 'pages/About';
+import Privacy from 'pages/Privacy';
+import Terms from 'pages/Terms';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path={ClientRoutesEnum.HOME} element={<Layout RenderComponent={Home} />} />
       <Route path={ClientRoutesEnum.PROFILE} element={<Layout RenderComponent={Profile} />} />
-      <Route path={ClientRoutesEnum.PROFILE} element={<Layout RenderComponent={Profile} />} />
       <Route path={ClientRoutesEnum.CREATE} element={<Layout RenderComponent={Creation} />} />
+      <Route path={ClientRoutesEnum.ABOUT} element={<Layout RenderComponent={About} />} />
+      <Route path={ClientRoutesEnum.PRIVACY} element={<Layout RenderComponent={Privacy} />} />
+      <Route path={ClientRoutesEnum.TERMS} element={<Layout RenderComponent={Terms} />} />
     </Routes>
   );
 }
